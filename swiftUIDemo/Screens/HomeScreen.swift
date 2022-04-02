563810ad2b5e3c56bf1f3b52db5ec870ed722de1
shokh <1mustafoman@gmail.com>
2022-04-02 14:12:01 +0500

    procom

---
+++ swiftUIDemo/Screens/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  swiftUIDemo
+//
+//  Created by Shohjahon Numonovich on 4/2/22.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text("Shohjahon Abdurahmonov").foregroundColor(Color.blue)
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
